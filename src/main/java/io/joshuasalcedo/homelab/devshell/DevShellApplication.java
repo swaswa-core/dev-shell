0c1dcc41ebcb0247753757e5acf34d5222c11d58
JoshuaSalcedo <dev@joshuasalcedo.io>
2025-07-22 00:40:53 +0800

    modified

---
+++ src/main/java/io/joshuasalcedo/homelab/devshell/DevShellApplication.java
@@ -0,0 +1,13 @@
+package io.joshuasalcedo.homelab.devshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevShellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/joshuasalcedo/homelab/devshell/DevShellApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joshuasalcedo.homelab.devshell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
